eb4c163ee467f7ae450880f41c4a3ba84e54a49e
jwmunn <contact@justinmunn.co>
2018-04-03 17:19:07 -0700

    Toggle clickable play button.

--- main.js
+++ main.js
@@ -1,12 +1,39 @@
-var scene = document.querySelector('#scene');
-var parallaxInstance = new Parallax(scene);
+(function(){
+  // Parallax
+  var scene = document.querySelector('#scene');
+  var parallaxInstance = new Parallax(scene);
 
-const triggers = document.querySelectorAll('.menu-button');
-triggers.forEach(trigger => trigger.addEventListener('click', handleClick));
+  const triggers = document.querySelectorAll('.menu-button');
+  triggers.forEach(trigger => trigger.addEventListener('mouseenter', handleEnter));
+  triggers.forEach(trigger => trigger.parentElement.addEventListener('mouseleave', handleLeave));
+  triggers.forEach(trigger => trigger.addEventListener('click', handleClick));
 
-function handleClick(e) {
-   e.preventDefault();
-   console.log('clicked!');
-   console.log(this.nextElementSibling);
-   this.nextElementSibling.classList.toggle('open');
-}
+  function handleEnter() {
+    this.nextElementSibling.classList.add('open');
+  }
+
+  function handleLeave() {
+    const c = this.childNodes[3];
+    console.log(c);
+    c.classList.remove('open');
+  }
+
+  function handleClick(e) {
+    e.preventDefault();
+    console.log('clicked!');
+    console.log(this.parentElement);
+    this.nextElementSibling.classList.toggle('open');
+  }
+
+  // Music
+  const musicTrigger = document.querySelector('.controler');
+  const circles = document.querySelector('.player-circles');
+  musicTrigger.addEventListener('click', playPause);
+
+  function playPause() {
+    this.classList.toggle('paused');
+    this.classList.toggle('playing');
+    circles.classList.toggle('show');
+  }
+
+})();
